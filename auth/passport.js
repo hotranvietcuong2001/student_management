57b3523c7f179343138018eeebbf8d847a80d3c5
Shin <matran4869@gmail.com>
2022-01-10 21:08:04 +0700

    passport

---
+++ auth/passport.js
@@ -0,0 +1,26 @@
+const passport = require('passport')
+  , LocalStrategy = require('passport-local').Strategy;
+
+const User = require("../src/app/models/User");
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    User.findOne({ username: username }, function (err, user) {
+      if (err) { return done(err); }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      if (validPassword(user, password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+    //   if (!user.validPassword(password)) {
+    //     return done(null, false, { message: 'Incorrect password.' });
+    //   }
+      return done(null, user);
+    });
+  }
+));
+
+function validPassword(user, password){
+    return user.password === password;
+}
